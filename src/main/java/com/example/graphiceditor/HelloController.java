a0f9a3f0bb402908e811319bd1874ed66cd67e61
BigMaestrov <nikita.egorov40k@gmail.com>
2022-10-25 20:11:12 +0400

    debug

--- src/main/java/com/example/graphiceditor/HelloController.java
+++ src/main/java/com/example/graphiceditor/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.graphiceditor;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -8,7 +10,14 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button1;
+
+    private int n=0;
+
+    @FXML
+    protected void clearCanvas(ActionEvent actionEvent){
+        System.out.println("Кнопка нажата");
+        n++;
+        button1.setText(""+n);
     }
 }
