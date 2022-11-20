3fdacb0c8d2b9a98901789e9b369947ebcef16cf
BigMaestrov <nikita.egorov40k@gmail.com>
2022-11-20 11:36:55 +0400

    refactor start methods

--- src/main/java/com/example/graphiceditor/HelloApplication.java
+++ src/main/java/com/example/graphiceditor/HelloApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,11 +13,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(HelloApplication.class.getResource("hello-view.fxml"));
+        AnchorPane root = fxmlLoader.load();
+        HelloController helloController = fxmlLoader.getController();
+        Scene scene = new Scene(root, 800, 600);
         stage.setTitle("GraphicEditor");
         stage.setScene(scene);
-        stage.show();
+        helloController.setPrimaryStage(stage);
     }
 
     public static void main(String[] args) {
